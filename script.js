866258a30cc2d2b2599c1125d25f64c479312e4f
Darrell Taylor <git@darrelltaylor.com>
2025-02-10 13:03:00 +0000

    feat: Initialize map display using Leaflet library

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const map = L.map('map').setView([53.8008, -1.5491], 12);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+});
