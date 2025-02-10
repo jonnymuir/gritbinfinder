64a895a9a0ebe545132b31059dd91c8fb4a345e5
Darrell Taylor <git@darrelltaylor.com>
2025-02-10 14:23:42 +0000

    feat: Pre-load green, blue, and red marker icons

--- script.js
+++ script.js
@@ -52,6 +52,21 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     searchAreaButton.addTo(map);
 
+    // Pre-load some icons
+    function createIcon(color) {
+        return new L.Icon({
+            iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+    }
+
+    var greenIcon = createIcon('green');
+    var blueIcon = createIcon('blue');
+    var redIcon = createIcon('red');
 
     getLocation();
 
